63cfeda84706ff7ef8dc9fe5a91802ad96da23b2
Riccardo Cogoni <cogonir@live.com>
2021-02-24 18:01:01 +0000

    fix reading data

--- models/laptops.js
+++ models/laptops.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const handleCEXData = require("../utils");
 
-const data = fs.readFileSync("./laptops.json", "utf8", (err, data) => {});
-
 const reqLaptops = () => {
+  const data = fs.readFileSync("./laptops.json", "utf8", (err, data) => {});
   return handleCEXData(JSON.parse(data)).then((rankedCPUs) => rankedCPUs);
 };
 
